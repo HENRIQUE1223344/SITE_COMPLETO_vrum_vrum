389f87ffc512cbb90f6ccac0b6446e26d0e3ae1b
HENRIQUE1223344 <130380853+HENRIQUE1223344@users.noreply.github.com>
2023-11-28 21:54:08 -0300

    Add files via upload

---
+++ SITE-main/SITE-main/site vrum vrum/javascript/script.js
@@ -0,0 +1,5 @@
+
+  
+  document.getElementById('meuBotao').addEventListener('click', function() {
+      alert('Bem-vindo ao Vrum Vrum, sua fonte definitiva para fichas técnicas de carros! Explore dados precisos e detalhados sobre uma variedade de modelos, desde desempenho até recursos inovadores. Descubra o que torna cada carro único e tome decisões informadas. Seja você um entusiasta de tecnologia automotiva ou simplesmente apaixonado por carros, estamos aqui para satisfazer sua curiosidade. Junte-se à nossa comunidade automotiva e mergulhe nas emoções que cada carro oferece. No Vrum Vrum, a jornada automotiva começa aqui. Explore. Descubra. Apaixone-se. (Apenas as marcas VW e FORD funcionam. O JS é essa mensagem, não consegui fazer em arquivo separado o js por conta de um erro)');
+  });
